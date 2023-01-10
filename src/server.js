6a0a826d9bea185506cb11d39c9e9ae324120ff6
Alexis <alexandrescu.alexandra.maria@gmail.com>
2023-01-10 15:51:58 +0200

    adds start script and dotenv

--- src/server.js
+++ src/server.js
@@ -1,12 +1,33 @@
 import express from "express";
 import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./api/authors/index.js";
+import createHttpError from "http-errors";
 
 //creates an express server; it is an object
 const server = express();
 
 //declaring the port (like 3000 for React)
-const port = 3001;
+// const port = 3001;
+const port = process.env.PORT;
+
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
+
+const corsOpts = {
+  origin: (origin, corsNext) => {
+    console.log("CURRENT ORIGIN: ", origin);
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      // If current origin is in the whitelist you can move on
+      corsNext(null, true);
+    } else {
+      // If it is not --> error
+      corsNext(
+        createHttpError(400, `Origin ${origin} is not in the whitelist!`)
+      );
+    }
+  },
+};
+
+server.use(cors(corsOpts));
 
 // If you do not add this line here BEFORE the endpoints, all req.body will be UNDEFINED
 server.use(express.json());
